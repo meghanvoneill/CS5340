47fa163acb8ae72015369ae0e4bacc3fecbc0811
Meghan V. O'Neill <u0759157@utah.edu>
2020-10-19 21:47:56 -0600

    Files added to project.

---
+++ program3/distsim.py
@@ -0,0 +1,13 @@
+
+# Author: Meghan V. O'Neill
+# For Program 3: Part 2 - Nearest-Neighbor Algorithm
+# Natural Language Processing, Fall 2020, Professor Riloff, University of Utah
+
+# Tested with: python3 distsim.py train.txt test.txt stopwords.txt 2
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ program3/lesk.py
@@ -0,0 +1,13 @@
+
+# Author: Meghan V. O'Neill
+# For Program 3: Part 1 - Lesk Algorithm
+# Natural Language Processing, Fall 2020, Professor Riloff, University of Utah
+
+# Tested with: python3 lesk.py test.txt definitions.txt stopwords.txt
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
